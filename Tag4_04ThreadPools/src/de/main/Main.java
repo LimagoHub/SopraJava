f9aa62334697da59de5794121e8d11656e7543d2
limagoHub <limagoHub@limago.de>
2020-11-12 19:33:51 +0100

    x

--- Tag4_04ThreadPools/src/de/main/Main.java
+++ Tag4_04ThreadPools/src/de/main/Main.java
@@ -1,7 +1,9 @@
 package de.main;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -11,30 +13,29 @@ public class Main {
 		final int availableProcessors = Runtime.getRuntime().availableProcessors();
 		
 		ExecutorService service = Executors.newFixedThreadPool(availableProcessors);
-		for (int i = 0; i <100; i++) {
-			service.execute(new Runnable() {
-				
-				@Override
-				public void run() {
-					// SEhr lange laufender Process
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					System.out.println("Hallo");
-					
-				}
-			});
-		}
+		
+		Future<Integer> ergebnis = service.submit(new MyCallable());
+		Future<Integer> ergebnis2 = service.submit(new MyCallable());
 		
 		service.shutdown();
 		System.out.println("Noch nicht....");
-		service.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+		//service.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+		
+		int sinnDesLebens = ergebnis.get();
+		int andererWert = ergebnis2.get();
 		
 		System.out.println("Fertisch!");
 
+	}
+	
+	static class MyCallable implements Callable<Integer> {
+
+		@Override
+		public Integer call() throws Exception {
+			Thread.sleep(1000);
+			return 42;
+		}
+		
 	}
 
 }
--- Tag4_05GroesseUebung/src/de/arrayfiller/ArrayFiller.java
+++ Tag4_05GroesseUebung/src/de/arrayfiller/ArrayFiller.java
@@ -2,12 +2,24 @@ package de.arrayfiller;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class ArrayFiller {
 
 	private static final int MAX_ARRAYSIZE = Integer.MAX_VALUE/2;
 	private final int [] data = new int[MAX_ARRAYSIZE];
+	private final int availableProcessors = Runtime.getRuntime().availableProcessors();
+	private int runningThreadCount=-1;
+	private ExecutorService service;
+	private int currentThreadNumber = -1;
 	
 	public static void main(String[] args) {
 		new ArrayFiller().go();
@@ -15,17 +27,121 @@ public class ArrayFiller {
 	}
 
 	private void go() {
-		Random random = new Random();
+		fillArrayWithRandomNumbers();
+		findMaxValueInArray();
+	}
+	
+	private void findMaxValueInArray() {
+		ExecutorService service = Executors.newFixedThreadPool(2);
 		
-		Instant start = Instant.now();
-		for(int i = 0 ;i < MAX_ARRAYSIZE; i ++) {
-			data[i] = random.nextInt();
+		List<Future<Integer>> futures = new ArrayList<>();
+		
+		futures.add(service.submit(new FindMaxValueInSegmentWorker(0, MAX_ARRAYSIZE / 2)));
+		futures.add(service.submit(new FindMaxValueInSegmentWorker(MAX_ARRAYSIZE / 2, MAX_ARRAYSIZE)));
+		
+		service.shutdown();
+		
+		int max = futures.stream().mapToInt(this::convert).max().getAsInt();
+		
+	}
+	
+	private int convert(Future<Integer> future) {
+		try {
+			return future.get();
+		} catch (Exception e) {
+			return Integer.MIN_VALUE;
+		} 
+	}
+
+	private void fillArrayWithRandomNumbers() {
+		for(runningThreadCount = 1; runningThreadCount <= availableProcessors + 1; runningThreadCount ++ ) {
+			startTimeMeasureDecorator();
 		}
-		Instant ende = Instant.now();
 		
-		Duration duration = Duration.between(start, ende);
+	}
+
+	private void startTimeMeasureDecorator() {
+		final Instant start = Instant.now();
+		startMultiThreadService();
+		final Instant ende = Instant.now();
+		final Duration duration = Duration.between(start, ende);
+		System.out.println(String.format("Duration with %s Threads was %s ms.", runningThreadCount, duration.toMillis()));
 		
-		System.out.println("Duration = " + duration.toMillis());
+	}
+
+	private void startMultiThreadService() {
+		try {
+			tryStartMultiThreadService();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void tryStartMultiThreadService() throws InterruptedException {
+		service = Executors.newFixedThreadPool(availableProcessors);
+		startThreads();
+		service.shutdown();
+		service.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+	}
+
+	private void startThreads() {
+		for(currentThreadNumber = 0; currentThreadNumber < runningThreadCount; currentThreadNumber ++) {
+			startFillSegmentWorker();
+		}
+		
+	}
+
+	private void startFillSegmentWorker() {
+		final int segmentSize = MAX_ARRAYSIZE / runningThreadCount;
+		final int start = currentThreadNumber * segmentSize;
+		final int end = (currentThreadNumber + 1) * segmentSize;
+		service.execute(new FillSegmentWorker(start, end));
+		
+	}
+
+	private class FillSegmentWorker implements Runnable {
+		private final int start, end;
+		
+
+		public FillSegmentWorker(final int start, final int end) {
+			this.start = start;
+			this.end = end;
+		}
+
+		@Override
+		public void run() {
+			final Random random = new Random();
+			for(int i = start; i < end; i ++) {
+				data[i] = random.nextInt();
+			}
+			
+		}
+		
+		
+	}
+	
+	private class FindMaxValueInSegmentWorker implements Callable<Integer> {
+
+		private final int start, end;
+		
+		
+		
+		public FindMaxValueInSegmentWorker(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+
+
+
+		@Override
+		public Integer call() throws Exception {
+			int max = data[start];
+			for(int i = start; i < end; i ++) {
+				if(max < data[i])
+					max = data[i];
+			}
+			return max;
+		}
 		
 	}
 
